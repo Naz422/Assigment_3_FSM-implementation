da4bcca7fcfc63c030dfdf5a134a38d988a02817
Naz422 <fa19-rcs-033@students.comsats.edu.pk>
2020-06-07 16:45:09 +0500

    Asssigmnt_3
    Assigmnt is completed

---
+++ mygame/Assets/FPS_controller.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_controller : MonoBehaviour
+{
+    public enum INIT_State { INITIAL, MOVING, DEAD, LEVEL_OVER };
+    [SerializeField]
+    private INIT_State currentState;
+
+    private FSM_Running run;
+
+    void Start()
+    {
+        run = GetComponent<FSM_Running>();
+        CurrentState = INIT_State.INITIAL;
+    }
+    public INIT_State CurrentState
+    {
+        get
+        {
+            return currentState;
+        }
+        set
+        {
+            currentState = value;
+            StopAllCoroutines();
+            switch (currentState)
+            {
+                case INIT_State.INITIAL:
+                    StartCoroutine(E_Initial(true));
+                    break;
+                case INIT_State.MOVING:
+                    StartCoroutine(E_Running(true));
+                    break;
+                case INIT_State.DEAD:
+                    StartCoroutine(E_Dead());
+                    break;
+            }
+          }
+    }
+    public IEnumerator E_Initial(bool tick)
+    {
+        
+            while (tick)
+            {
+                if (run.running)
+                {
+                    tick = false;
+                    break;
+                    CurrentState = INIT_State.MOVING;
+                }
+            }
+
+            yield return null;
+    
+    }
+    public IEnumerator E_Running(bool tick)
+    {
+       
+            while (tick)
+            {
+                if (run.collide)
+                {
+                    if (run.energatic > 0)
+                    {
+                        tick = false;
+                        
+                        break;
+                        CurrentState = INIT_State.DEAD;
+                    }
+                    else
+                    {
+                        tick = false;
+                        CurrentState = INIT_State.LEVEL_OVER;
+                    }
+
+                }
+            }
+        
+        yield return null;
+    }
+    public IEnumerator E_Dead()
+    {
+
+        yield return new WaitForSeconds(8f);
+    }
+    void Update()
+    {
+
+
+    }
+}
+// Start is called before the first frame update
---
+++ mygame/Assets/FSM_Running.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FSM_Running : MonoBehaviour
+{
+    public bool collide;
+    public float energatic;
+    public bool moving;
+    public bool running;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.transform.tag == "cyl")
+        {
+            collide = true;
+            energatic = energatic - 15;
+
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        energatic = 100;
+        running = false;
+        collide = false;
+        moving = false;
+        // fpc = GameObject.FindObjectOfType<FirstPersonController>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Change state
+        if (GameObject.FindObjectOfType<FirstPersonController>().m_CharacterController.velocity == Vector3.zero)
+        {
+
+            running = false;
+            moving = true;
+
+
+
+        }
+        else if (GetComponent<FirstPersonController>().m_IsWalking)
+        {
+
+            running = true;
+            moving = false;
+
+        }
+    }
+}
+
+// Start is called before the first frame update
+
+
+// Update is called once per frame
+
